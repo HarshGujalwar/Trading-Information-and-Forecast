aa50d4edafa44880faed1bd0e98fe80558ecea35
Harshlko31 <103877387+Harshlko31@users.noreply.github.com>
2023-07-17 22:10:37 +0530

    Update predictors.py

--- Stock_Predictor/predictors.py
+++ Stock_Predictor/predictors.py
@@ -30,26 +30,3 @@ def predict(tick):
     acc=math.floor(acc)
     return ans,acc
 
-def accuracy(tick):
-    import yfinance as yf
-    import pandas as pd
-    import math
-    ticker = yf.Ticker(tick)
-    data = ticker.history(period="10y")
-    data = data.loc["1990-01-01":].copy()
-    del data["Dividends"]
-    del data["Stock Splits"]
-    data["Tomorrow"] = data["Close"].shift(-1)
-    data["Target"] = (data["Tomorrow"]>data["Close"].astype(int))
-    from sklearn.ensemble import RandomForestClassifier
-    model = RandomForestClassifier(n_estimators=100,min_samples_split=100,random_state=1)
-    train=data.iloc[:-100]
-    test = data.iloc[-100:]
-    predictors= ["Close","Volume","Open","High","Low"]
-    model.fit(train[predictors],train["Target"])
-    preds = model.predict(test[predictors])
-    preds=pd.Series(preds,index=test.index)
-    from sklearn.metrics import precision_score
-    acc = precision_score(test["Target"],preds)
-    acc*=100
-    return math.floor(acc)
